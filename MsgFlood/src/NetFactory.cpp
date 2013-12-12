4d2ba0f82eb402d2d6d21dd1df371b9c34c54bc7
Dan Presley <dpresley@geniedb.com>
2013-12-12 11:46:09 -0800

    fix namespace

--- MsgFlood/src/NetFactory.cpp
+++ MsgFlood/src/NetFactory.cpp
@@ -4,8 +4,12 @@
 using namespace ZmqNet;
 using namespace Model;
 
+namespace Model {
+
 Net* createNet(std::string protocol) {
 	if (protocol != "zmq")
 		throw("only zmq protocol is currently supported");
 	return new ZmqNetImpl();
+}
+
 }
